dbed7cc0c647bb21175738541bbb12bdcd5f6cb5
Carnell <92267723+c-poteat@users.noreply.github.com>
2021-12-05 20:55:00 -0500

    Updated part 1 printed out a print statement

--- src/main/java/io/zipcoder/polymorphism/TalkingPetsApp.java
+++ src/main/java/io/zipcoder/polymorphism/TalkingPetsApp.java
@@ -5,27 +5,19 @@ public class TalkingPetsApp {
     private final IOConsole console = new IOConsole(AnsiColor.RED);
 
     public void run() {
-    getPetInputFromUser();
-    getPetTypeFromUser();
-    getPetNameFromUser();
+        getWelcomeScreen();
+        String numberOfPetInput = console.getStringInput("\nHow many pets do you have?");
+        String typeOfPetInput = console.getStringInput("What are the type of pets you have?");
+        String nameOfPetInput = console.getStringInput("Whare the names of your pets?");
+        System.out.println("You have " + numberOfPetInput + " pet(s)." + " The types of pet(s) you have are " + typeOfPetInput
+                + "." + " The name of your pet(s) are " + nameOfPetInput + ".");
 
     }
 
-    private String getPetInputFromUser() {
+    private String getWelcomeScreen() {
         return console.getStringInput(new StringBuilder()
-                .append("Welcome to Talking Pets!\n")
-                .append("\nPlease tell us how many Pets you have?")
-                .toString());
-    }
-
-    private String getPetTypeFromUser() {
-        return console.getStringInput(new StringBuilder()
-                .append("What kind of Pets do you have?")
-                .toString());
-    }
-    private String getPetNameFromUser() {
-        return console.getStringInput(new StringBuilder()
-                .append("What are the names of your Pets?")
+                .append("Welcome to Talking Pets!")
+                .append("\nPlease press enter to begin")
                 .toString());
     }
 }
